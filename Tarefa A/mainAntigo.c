08f537bbd550e498f9b1a321da5487eebf152b46
Samuel Sena <40075896+Globson@users.noreply.github.com>
2019-10-31 14:16:29 -0300

    LIVRE DE REALLOC!

--- Tarefa A/mainAntigo.c
+++ Tarefa A/mainAntigo.c
@@ -7,7 +7,9 @@ int main(int argc, char const *argv[]) {
   FILE *f;
   char NomeArquivo[20];
   // char NomeArquivo[20]="Arquivo.txt"; //retirar dps
-  int QuantLinhas=-1;
+  int Contador=0,QuantLinhas;
+  //int Valores[5050]; //5050 é o valor da soma dos 100 primeiros numeros de 1 a 100;
+  int *Valores = (int*)malloc(sizeof(int)); //utilizei um vetor dinamico para diminuir gasto exagerado de memoria em vetor de 5050 posicoes.
   printf("\nDigite o nome do arquivo que deseja abrir:");    //Abrindo arquivo//
   scanf("%s",NomeArquivo);
   f = fopen(NomeArquivo,"r");
@@ -20,17 +22,28 @@ int main(int argc, char const *argv[]) {
         Matriz[k][l]=0;
       }
     }*/
-    for(int i=0;i<100;i++){
-      for(int j=0;j<i+1;j++){
-         fscanf(f,"%d",&Matriz[i][j]);
-         if(feof(f)){
-           break;
-         }
-      }
-      QuantLinhas++;
-      if(feof(f)){
-        break;
-      }
+    while(!feof(f)){
+      fscanf(f,"%d",&Valores[Contador]);
+      Contador++;
+      Valores = (int*)realloc(Valores,sizeof(int)*(Contador+1));
+    }
+    printf("\n\tContador: %d\n",Contador); //Para debugar
+    Contador-=1;  //Descontado 1 devido ao feof() rodar uma ultima vez ao quebrar a ultima linha.
+    int Quant=1; //Valor inicial para multiplas subtracoes.
+    do{
+      Contador-=Quant;
+      Quant++;
+    }while(Contador!=0);
+
+    QuantLinhas = Quant-1; //Quantidade de linhas da piramide, decontando -1 => valor inicial.
+    printf("\n\tQuantLinhas: %d\n",QuantLinhas); //Para debugar
+
+    Contador = 0; //Setando para 0 parar poder tranferir de vetor para matriz.
+    for(int i=0;i<QuantLinhas;i++){
+        for(int j=0;j<i+1;j++){
+          Matriz[i][j]=Valores[Contador];
+          Contador++;
+        }
     }
     //Printando matriz para testar
     for(int i=0;i<QuantLinhas;i++){
