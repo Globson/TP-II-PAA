68b1cd213c20f26a7e8fd4d5617efb563d6de962
Samuel <samuelpedro1999@gmail.com>
2019-11-05 22:08:28 -0300

    Modifiquei um indice

--- Tarefa A/Sources/DeTrasPraFrente.c
+++ Tarefa A/Sources/DeTrasPraFrente.c
@@ -1,7 +1,7 @@
 #include "../Headers/DeTrasPraFrente.h"
 
 int DeTrasPraFrente(int Matriz[][100],int QuantLinhas){
-  for(int i=QuantLinhas-1;i>=0;i--){
+  for(int i=QuantLinhas-2;i>=0;i--){    //QuantLinhas-2 devido a começar do 0;
     for(int j=0;j<i+1;j++){
       Matriz[i][j] += Maior(Matriz[i+1][j],Matriz[i+1][j+1]);
     }
@@ -10,7 +10,7 @@ int DeTrasPraFrente(int Matriz[][100],int QuantLinhas){
 }
 
 int TopDeTrasPraFrente(int Matriz[][100],int QuantLinhas){
-  int MatrizAux[100][100];
+  int MatrizAux[QuantLinhas][100];
   CopiaMatriz(Matriz,MatrizAux,QuantLinhas);
   return DeTrasPraFrente(MatrizAux,QuantLinhas);
 }
